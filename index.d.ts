f5984f2baeb006b6243388e39a252f6a8bb37222
Dominic Lee <34794189+dominictwlee@users.noreply.github.com>
2019-06-21 15:18:13 +0100

    fix: Allow null delay type def

--- index.d.ts
+++ index.d.ts
@@ -1,23 +1,23 @@
 export type TimerStatesAndDispatchers = {
-  setDelay: (type: string, delay: number) => void;
-  intervalDelay: number;
-  timeoutDelay: number;
+  setDelay: (type: string, delay: number | null) => void;
+  intervalDelay: number | null;
+  timeoutDelay: number | null;
   clearAll: () => void;
 };
 
 /**
  *
  * @param intervalCb Callback function to be executed in setInterval
- * @param iDelay Delay duration for interval in ms
- * @param tDelay Delay duration for timeout in ms
+ * @param iDelay Initial delay duration for interval in ms
+ * @param tDelay Initial delay duration for timeout in ms
  * @param timeoutCb Callback function to be executed in setTimeout
  * @returns Time delay states and functions to update them
  */
 declare function useTimeoutableInterval(
   intervalCb: () => void,
-  iDelay: number,
+  iDelay: number | null,
   timeoutCb?: () => void,
-  tDelay?: number
+  tDelay?: number | null
 ): TimerStatesAndDispatchers;
 
 export default useTimeoutableInterval;
